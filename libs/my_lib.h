61f962af8546006232ad0d0d8f755a89a18cf428
Nathan-N7 <nathan.rod1710@gmail.com>
2025-03-26 16:32:09 -0300

    main criada e funcionando

---
+++ libs/my_lib.h
@@ -0,0 +1,9 @@
+#ifndef MY_LIB_H
+# define MY_LIB_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_strcmp(const char *s1, const char *s2);
+
+#endif
---
+++ main/minishell.c
@@ -0,0 +1,39 @@
+#include "../libs/minishell.h"
+#include "../libs/my_lib.h"
+//gcc minishell.c -lreadline -lhistory
+int ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int	i;
+	unsigned char	*s;
+
+	s = (unsigned char *)s1;
+	i = 0;
+	while (s[i] && s2[i] && s[i] == s2[i])
+	{
+		i++;
+	}
+	return (s[i] - s2[i]);
+}
+
+int main(int ac, char **av)
+{
+    char    *input;
+
+    (void)av;
+    if (ac != 1)
+    {
+        printf("Error");
+        return (0);
+    }
+    while (1)
+    {
+        input = readline("minishell$> ");
+        if (!input || ft_strcmp(input, "exit") == 0)
+             break;
+        if (*input)
+            add_history(input);
+        printf ("%s\n", input);
+        free (input);
+    }
+    return (0);
+}
